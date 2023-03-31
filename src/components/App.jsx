b7baa698ffed0e3264ce38478aba94433f4cc741
Nataliia Svystula <natashasvistula@gmail.com>
2023-03-31 21:53:17 +0300

    add BG

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,14 @@ import { GlobalStyle } from './GlobalStyles';
 
 import { Signin } from 'pages/Signin/Signin';
 
+import {MainPage} from 'pages/MainPage/MainPage'
+
 export const App = () => {
   return (
     <>
       <Routes>
-        <Route path="/" element={<Signin />}></Route>
+        <Route path="/" element={<Signin />} />
+        <Route path="/main" element={<MainPage />} />
       </Routes>
       <GlobalStyle />
     </>
