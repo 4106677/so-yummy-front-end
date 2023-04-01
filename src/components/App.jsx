2859c64c39c236e16d99f1e9f65d50eb21d2b8be
Yaroslav Oliinyk <4106677@gmail.com>
2023-04-01 23:16:15 +0300

    upd yarn lock

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,18 @@ import { Route, Routes } from 'react-router-dom';
 
 import { GlobalStyle } from './GlobalStyles';
 
-
-import { Signin } from 'pages/Signin/Signin';
+// import { Signin } from 'pages/Signin/Signin';
 import { SearchPage } from 'pages/SearchPage/SearchPage';
 import { RegisterPage } from 'pages/Register/Register';
-import {MainPage} from 'pages/MainPage/MainPage'
+// import {MainPage} from 'pages/MainPage/MainPage'
 import { WellcomePage } from 'pages/WellcomePage/WellcomePage';
-
 
 export const App = () => {
   return (
     <>
       <Routes>
-            <Route path="/" element={<WellcomePage />}></Route>
-               <Route path="/register" element={<RegisterPage />}></Route>
+        <Route path="/" element={<WellcomePage />}></Route>
+        <Route path="/register" element={<RegisterPage />}></Route>
         <Route path="/search" element={<SearchPage />}></Route>
       </Routes>
       <GlobalStyle />
