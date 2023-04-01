0e130e93f63740128c2c1fd876bb8612e86e7015
EkaterinaKononenko <kaktysik4@ukr.net>
2023-04-01 23:37:28 +0300

    update css

--- src/pages/SearchPage/SearchPage.styled.js
+++ src/pages/SearchPage/SearchPage.styled.js
@@ -4,17 +4,14 @@ export const SearchHeader = styled.h1`
   font: 'Poppins';
   font-weight: 600;
   font-size: 28px;
-  margin-left: 1rem;
   margin-bottom: 3.125rem;
 
   @media (min-width: 768px) {
     font-size: 32px;
-    margin-left: 2rem;
     margin-bottom: 2.5rem;
   }
   @media (min-width: 1440px) {
     font-size: 44px;
-    margin-left: 6.25rem;
     margin-bottom: 3.125rem;
   }
 `;
