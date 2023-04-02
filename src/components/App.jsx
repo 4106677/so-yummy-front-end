687f7a9fdaf2e075db99a639721b17d997c87cf8
Yurov Dmitro <yurov37@gmail.com>
2023-04-02 19:46:53 +0300

    add Recipe page components and style

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,8 @@ import { Route, Routes } from 'react-router-dom';
 import { GlobalStyle } from './GlobalStyles';
 
 import { Signin } from 'pages/Signin/Signin';
-import {SearchPage} from 'pages/SearchPage/SearchPage';
+import { SearchPage } from 'pages/SearchPage/SearchPage';
+import { RecipePage } from 'pages/RecipePage/RecipePage';
 
 export const App = () => {
   return (
@@ -11,6 +12,7 @@ export const App = () => {
       <Routes>
         <Route path="/" element={<Signin />}></Route>
         <Route path="/SearchPage" element={<SearchPage />}></Route>
+        <Route path="/recipe/:recipeId" element={<RecipePage />}></Route>
       </Routes>
       <GlobalStyle />
     </>
--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -3,7 +3,6 @@ import { createGlobalStyle } from 'styled-components';
 export const GlobalStyle = createGlobalStyle`
   body {
   margin: 0;
-
     padding: 0;
   font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto', 'Oxygen',
     'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue',
