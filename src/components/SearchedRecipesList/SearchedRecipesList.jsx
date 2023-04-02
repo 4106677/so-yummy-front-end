b65886160dfc0734a6fef682f3ae8b7451ea8de7
EkaterinaKononenko <kaktysik4@ukr.net>
2023-04-02 15:24:13 +0300

    add not found

--- src/components/SearchedRecipesList/SearchedRecipesList.jsx
+++ src/components/SearchedRecipesList/SearchedRecipesList.jsx
@@ -1,18 +1,30 @@
-import { ListWrap } from "./SearchedRecipesList.styled";
+import {
+  ListWrap,
+  NotFoundPhoto,
+  NotFoundWrap,
+  NotFoundText,
+} from './SearchedRecipesList.styled';
 
 export const SearchedRecipesList = ({ items = []}) => {
 console.log(items);
   
   return (
     <div>
-      <ListWrap>
-        {items.map(item => (
-          <li key={item.id}>
-            <img src={items.photo} alt={items.titles}></img>
-            <p>{item.titles}</p>
-          </li>
-        ))}
-      </ListWrap>
+      {items.length === 0 ? (
+        <NotFoundWrap>
+          <NotFoundPhoto></NotFoundPhoto>
+          <NotFoundText>Try looking for something else...</NotFoundText>
+        </NotFoundWrap>
+      ) : (
+        <ListWrap>
+          {items.map(item => (
+            <li key={item.id}>
+              <img src={items.photo} alt={items.titles}></img>
+              <p>{item.titles}</p>
+            </li>
+          ))}
+        </ListWrap>
+      )}
     </div>
   );
 }
--- src/components/SearchedRecipesList/SearchedRecipesList.styled.js
+++ src/components/SearchedRecipesList/SearchedRecipesList.styled.js
@@ -1,4 +1,62 @@
 import styled from "styled-components";
+import notFoundSearchMob from "../../images/SearchPage/search_not_found_mob_1x.png"
+import notFoundSearchMobRetina from "../../images/SearchPage/search_not_found_mob_2x.png"
+import notFoundSearchTab from '../../images/SearchPage/search_not_found_tab_1x.png';
+import notFoundSearchTabRetina from '../../images/SearchPage/search_not_found_tab_2x.png';
+import notFoundSearchDesk from '../../images/SearchPage/search_not_found_tab_1x.png';
+import notFoundSearchDeskRetina from '../../images/SearchPage/search_not_found_tab_2x.png';
 
 export const ListWrap = styled.ul`
 text-align: center;`
+
+export const NotFoundPhoto = styled.div`
+  background-size: cover;
+  display: inline-block;
+  height: 133px;
+  width: 208px;
+  background-repeat: no-repeat;
+  background-image: url(${notFoundSearchMob});
+  @media screen and (min-device-pixel-ratio: 2),
+    (min-resolution: 192dpi),
+    (min-resolution: 2dppx) {
+    background-image: url(${notFoundSearchMobRetina});
+  }
+
+  @media screen and (min-width: 768px) {
+    height: 225px;
+    width: 350px;
+    background-image: url(${notFoundSearchTab});
+    @media screen and (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${notFoundSearchTabRetina});
+    }
+  }
+
+  @media screen and (min-width: 1440px) {
+    background-image: url(${notFoundSearchDesk});
+    @media screen and (min-device-pixel-ratio: 2),
+      (min-resolution: 192dpi),
+      (min-resolution: 2dppx) {
+      background-image: url(${notFoundSearchDeskRetina});
+    }
+  }
+`;
+
+export const NotFoundWrap = styled.div`
+  text-align: center;
+  margin-top: 3.125rem;
+`
+
+export const NotFoundText = styled.p`
+  font-family: 'Poppins';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  color: #000000;
+  opacity: 0.5;
+
+  @media screen and (min-width: 768px) {
+    font-size: 24px;
+  }
+`;
