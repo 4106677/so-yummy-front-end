f58e68f4f96d2d159b240b303e77231173be7101
Yaroslav <104924953+4106677@users.noreply.github.com>
2023-04-03 08:27:04 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,6 +8,10 @@ import { RegisterPage } from 'pages/Register/Register';
 import { SigninPage } from 'pages/Signin/Signin';
 import { MainPage } from 'pages/MainPage/MainPage';
 import { WellcomePage } from 'pages/WellcomePage/WellcomePage';
+import { AddRecipePage } from 'pages/AddRecipe/AddRecipePage';
+import { RecipePage } from 'pages/RecipePage/RecipePage';
+
+
 
 export const App = () => {
   return (
@@ -18,10 +22,10 @@ export const App = () => {
         <Route path="/signin" element={<SigninPage />}></Route>
         <Route path="/main" element={<MainPage />}></Route>
         <Route path="/search" element={<SearchPage />}></Route>
+  <Route path="/recipe/:recipeId" element={<RecipePage />}></Route>
+        <Route path="/add-recipe" element={<AddRecipePage />}></Route>
       </Routes>
       <GlobalStyle />
     </>
   );
 };
-
-
