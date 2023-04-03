c9d1d8bc17079a72162f0e46284643f7ff2649ee
Yaroslav <104924953+4106677@users.noreply.github.com>
2023-04-03 07:57:45 +0300

    Update GlobalStyles.js

--- src/components/GlobalStyles.js
+++ src/components/GlobalStyles.js
@@ -28,4 +28,13 @@ ul {
  a {
     text-decoration: none;
    }
+   
+    h1, h2, h3, h4, h5, h6, p, button {
+    margin: 0;
+    padding: 0;
+  }
+
+  *, *::before, *::after {
+    box-sizing: border-box;
+  }
 `;
