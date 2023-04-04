21d306eb53f4eb09a763440dcaa96186e1dc49e7
Yaroslav <104924953+4106677@users.noreply.github.com>
2023-04-04 15:44:19 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,18 +8,22 @@ import { RegisterPage } from 'pages/Register/Register';
 import { SigninPage } from 'pages/Signin/Signin';
 import { MainPage } from 'pages/MainPage/MainPage';
 import { WellcomePage } from 'pages/WellcomePage/WellcomePage';
+import { AddRecipePage } from 'pages/AddRecipe/AddRecipePage';
+import { RecipePage } from 'pages/RecipePage/RecipePage';
 
 import { ToastContainer } from 'react-toastify';
 
 export const App = () => {
   return (
     <>
-      <Routes>
+ <Routes>
         <Route path="/" element={<WellcomePage />}></Route>
         <Route path="/register" element={<RegisterPage />}></Route>
         <Route path="/signin" element={<SigninPage />}></Route>
         <Route path="/main" element={<MainPage />}></Route>
         <Route path="/search" element={<SearchPage />}></Route>
+        <Route path="/recipe/:recipeId" element={<RecipePage />}></Route>
+        <Route path="/add-recipe" element={<AddRecipePage />}></Route>
       </Routes>
       <GlobalStyle />
       <ToastContainer autoClose={3000} />
