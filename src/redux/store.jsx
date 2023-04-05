1dc3829008c6fe8dc01e92fd78e458add6a9cb61
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-05 16:26:39 +0300

    add redux auth

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,8 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
 import { mainRecipeReduser } from './mainRecipes/slice';
+import { authReducer } from './auth/slice';
+
+const authPersistConfig = {
+  key: 'auth',
+  storage,
+  whitelist: ['token'],
+};
+
+const persistedAuthReducer = persistReducer(authPersistConfig, authReducer);
 
 export const store = configureStore({
   reducer: {
+    auth: persistedAuthReducer,
     recipe: mainRecipeReduser,
   },
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
+
+export const persistor = persistStore(store);
