428b614d54c6ceb92841f765cedeae966196df4e
Yurii <Rapacta@gmail.com>
2023-04-04 23:42:13 -0600

    footer-v3-fixed

--- src/components/Footer/Navigation/Navigation.jsx
+++ src/components/Footer/Navigation/Navigation.jsx
@@ -4,23 +4,43 @@ import { SubscribeForm } from './SubscribeForm/SubscribeForm';
 import { FollowUs } from './FollowUs/FollowUs';
 import {
     LogoFooterLink,
-    LogoLinkSpan
+    LogoLinkSpan,
+    AboutList,
+    AboutItem,
+    WrapperLogoAboutNav,
+    WrapperLogoNavSub
 } from './Navigation.styled';
 
 export const Navigation = () => {
     return (
         <div>
-            <div>
-                <LogoFooterLink to="/">
-                    <LogoFooterSvg />
-                    <LogoLinkSpan>
-                        So Yummy
-                    </LogoLinkSpan>
-                </LogoFooterLink>
-                {/* <p>About</p> */}
-                <Nav/>
+            <WrapperLogoNavSub>
+                <WrapperLogoAboutNav>
+                    <div>
+                    <LogoFooterLink to="/">
+                        <LogoFooterSvg />
+                        <LogoLinkSpan>
+                            So Yummy
+                        </LogoLinkSpan>
+                    </LogoFooterLink>
+                    
+                    <AboutList>
+                        <AboutItem>Database of recipes that can be replenished </AboutItem>
+                        <AboutItem>Flexible search for desired and unwanted ingredients</AboutItem>
+                        <AboutItem>Ability to add your own recipes with photos</AboutItem>
+                        <AboutItem>Convenient and easy to use</AboutItem>
+                        </AboutList>
+                    </div>
+
+                    <Nav />
+                    
+                </WrapperLogoAboutNav>
+
                 <SubscribeForm />
-            </div>
+                
+            </WrapperLogoNavSub>
+
+            
             <FollowUs/>
         </div>
     )
