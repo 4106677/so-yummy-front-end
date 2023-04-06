d875087f3db1e7b868de789bd4c7285755cfee93
Nataliia Svystula <105055646+Nataliia0202@users.noreply.github.com>
2023-04-06 23:07:57 +0300

    Update TheMealAPI.js

--- src/services/TheMealAPI.js
+++ src/services/TheMealAPI.js
@@ -9,4 +9,59 @@ export const getSetOfCategoriestAPI = () => {
     return data.data;
   });
 };
+export const getCategoryListAPI = () => {
+  return axios.get('/recipes/category-list ').then(({ data }) => {
+    return data;
+  });
+};
+
+export const getAllRecipesByCategoryAPI = category => {
+  return axios.get(`/recipes/byCategory/${category}`).then(({ data }) => {
+    return data;
+  });
+};
+
+export const getLimitedRecipesByCategoryAPI = (category, limit) => {
+  return axios.get(`/recipes/${category}/${limit}`).then(({ data }) => {
+    return data;
+  });
+};
+
+export const getOneRecipeByIdAPI = id => {
+  return axios.get(`/recipes/byId/${id}`).then(({ data }) => {
+    return data;
+  });
+};
+
+export const getPopularRecipesAPI = () => {
+  return axios.get('/recipes/popular').then(({ data }) => {
+    return data;
+  });
+};
+
+export const getRecipesByQueryAPI = (query, page = 1, per_page = 12) => {
+  return axios
+    .get(`/recipes/search/${query}?page=${page}&per_page=${per_page}`)
+    .then(({ data }) => {
+      return data;
+    });
+};
+
+export const getAllIngredientsAPI = () => {
+  return axios.get('/ingredients/list').then(({ data }) => {
+    return data;
+  });
+};
+
+export const getRecipesByIngredientAPI = (
+  ingredient,
+  page = 1,
+  per_page = 12
+) => {
+  return axios
+    .get(`/ingredients/${ingredient}?page=${page}&per_page=${per_page}`)
+    .then(({ data }) => {
+      return data;
+    });
+};
 
