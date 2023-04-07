4036aeffd016b71da8b8cc40f972fc9fbfe1e82a
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-07 11:44:18 +0300

    add header/footer

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,18 +1,22 @@
-import {
-    NFtitle,
-    NFText,
-    NFImg,
-    NFDivWrap
-} from './NotFound.styled';
+import { Header } from 'components/Header/Header';
+import { Footer } from 'components/Footer/Footer';
+
+import { NFtitle, NFText, NFImg, NFDivWrap } from './NotFound.styled';
+import { DecorativeSquare } from '../MyRecipesPage/MyRecipePage.styled';
 
 export const NotFound = () => {
-    return (
-        <>
-        <NFDivWrap>
-            <NFImg/>
-            <NFtitle>We are sorry,</NFtitle>
-            <NFText>but the page you were looking for can’t be found..</NFText>
-        </NFDivWrap>
-        </>
-    )
-}
+  return (
+    <>
+      <Header />
+      <DecorativeSquare data-1 color="#8BAA36" top="3.375rem" left="14.75rem" />
+      <DecorativeSquare data-2 top="8.5rem" left="44.125rem" />
+      <DecorativeSquare data-3 color="#8BAA36" top="4.125rem" right="4.5rem" />
+      <NFDivWrap>
+        <NFImg />
+        <NFtitle>We are sorry,</NFtitle>
+        <NFText>but the page you were looking for can’t be found..</NFText>
+      </NFDivWrap>
+      <Footer />
+    </>
+  );
+};
