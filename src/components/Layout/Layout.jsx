d51b98235cd518f4f29f25288677f035360f57a5
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-07 16:10:02 +0300

    moved not found page to component

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,15 +1,16 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Header } from 'components/Header/Header';
-import { Footer } from 'components/Footer/Footer'
+import { Footer } from 'components/Footer/Footer';
+import { Loader } from 'components/Loader/Loader';
 
 export const Layout = () => {
   return (
     <>
-      <Suspense>
-      <Header/>
+      <Suspense fallback={<Loader />}>
+        <Header />
         <Outlet />
-      <Footer/>
+        <Footer />
       </Suspense>
     </>
   );
