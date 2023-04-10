b07a66e760801b39f8f3cd5fa1083041ed3de475
DidorchukAlexandr <104354949+DidorchukAlexandr@users.noreply.github.com>
2023-04-11 02:46:49 +0300

    add fix

--- src/components/CategoriesList/CategoriesList/CategoriesList.jsx
+++ src/components/CategoriesList/CategoriesList/CategoriesList.jsx
@@ -1,65 +1,21 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import 'react-tooltip/dist/react-tooltip.css';
 
 import {
-  CardWrapper,
   CategoryItem,
   CategoryList,
   Title,
 } from './CategoriesList.styled';
-import { getCategoryList, getAllRecipesByCategory } from '../../../redux/mainRecipes/operations';
-import { getFullCategoryList, getAllRecipes } from '../../../redux/mainRecipes/selectors';
-import { Link } from 'react-router-dom';
-import { CardImg, CardTitle, CardWrap } from '../RecipeList/RecipeList.styled';
-import NoImage from '../../../images/default.jpg';
 
-
-
-export function CategoriesList() {
-  const categories = useSelector(getFullCategoryList);
+export function CategoriesList({ categories = [] }) {
   console.log(categories);
-  const meals = useSelector(getAllRecipes);
-  console.log(meals);
-    const dispatch = useDispatch();
-    
-  useEffect(() => {
-      
-    dispatch(getCategoryList());
-    
-  }, [dispatch]);
-  
-  useEffect(() => {
-      const category = 'beef';
-    dispatch(getAllRecipesByCategory(category));
-    
- }, [dispatch]);
-   
 
-  const numCard = 8;
-    
+const categoryCreator = categories.map(({ name, idx }) => (
+   <CategoryItem key={idx}>
+      <Title>{name}</Title>
+    </CategoryItem>
+  ));
+  
   return (
-       <CategoryList>
-        {categories.map(({ name }, idx) => (
-          <CategoryItem key={idx}>
-            <Title onClick={() => dispatch(getAllRecipesByCategory(name))}>{name}</Title>
-            <CardWrapper>
-              {meals.slice(0, numCard).map(({ _id, title, preview }) => (
-                <CardWrap key={_id}>
-                  <Link to={`/recipes/byId/${_id}`}>
-                    <CardImg
-                      src={preview ? preview : NoImage }
-                      alt={title}
-                    />
-                    <CardTitle>{title}</CardTitle>
-                  </Link>
-                </CardWrap>
-              ))}
-            </CardWrapper>
-            
-          </CategoryItem>
-        ))}
-      </CategoryList>
+       <CategoryList>{categoryCreator}</CategoryList>
     );
 };
 
--- src/components/CategoriesList/RecipeList/RecipeList.jsx
+++ src/components/CategoriesList/RecipeList/RecipeList.jsx
@@ -1,17 +1,26 @@
 import { Link } from 'react-router-dom';
 
-import { CardImg, CardTitle, CardWrap } from './RecipeList.styled';
+import { CardWrapper, CardImg, CardTitle, CardWrap } from './RecipeList.styled';
 import NoImage from '../../../images/default.jpg';
 
-export const RecipeList = ({ _id, title, preview }) => {
+export const RecipeList = ({ meals = [] }) => {
+  console.log(meals);
+  const recipeCreator = meals.map(({ _id, title, preview }) => (
+                <CardWrap key={_id}>
+                  <Link to={`/recipes/byId/${_id}`}>
+                    <CardImg
+                      src={preview ? preview : NoImage }
+                      alt={title}
+                    />
+                    <CardTitle>{title}</CardTitle>
+                  </Link>
+                </CardWrap>
+              ))
+
   return (
-    <CardWrap>
-      <Link to={`/recipes/byId/${_id}`}>
-        <CardImg src={preview ? preview : { NoImage }} alt={title} />
-        <CardTitle >{title}</CardTitle>
-        
-      </Link>
-    </CardWrap>
+    <>
+          <CardWrapper>{recipeCreator}</CardWrapper>
+    </>
   );
 };
 
