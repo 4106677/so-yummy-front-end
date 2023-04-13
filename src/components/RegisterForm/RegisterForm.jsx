f58193fdfc2b692a4d7510d60d7a8c69226847dd
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-04-13 21:48:54 +0300

    Update RegisterForm.jsx

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { Formik, Form } from 'formik';
 import { useDispatch } from 'react-redux';
-// import { toast } from 'react-toastify';
 
 import {
   BackgroundImage,
